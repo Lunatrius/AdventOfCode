c34276cc2204f11bcc0b68860b3334b3ea0f4bd6
Lunatrius <jadran.kotnik@gmail.com>
2015-12-25 00:13:46 +0100

    Day 5, part 2

--- 5/main.py
+++ 5/main.py
@@ -2,14 +2,20 @@
 import fileinput
 
 
+def pair2(string):
+    for i in range(0, len(string)):
+        for j in range(i + 2, len(string)):
+            if string[i:i+2] == string[j:j+2]:
+                return True
+
+    return False
+
+
 def is_nice(string):
-    if sum(string.count(c) for c in 'aeiou') < 3:
+    if not pair2(string):
         return False
 
-    if not any(1 for x in zip(string, string[1:]) if x[0] == x[1]):
-        return False
-
-    if any(s in string for s in ['ab', 'cd', 'pq', 'xy']):
+    if not any(1 for x in zip(string, string[2:]) if x[0] == x[1]):
         return False
 
     return True
