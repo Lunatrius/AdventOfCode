497e19a912360ca3c7138150215d7050061d3c94
Lunatrius <jadran.kotnik@gmail.com>
2015-12-24 22:55:01 +0100

    Day 1, part 2

--- 1/main.py
+++ 1/main.py
@@ -3,8 +3,13 @@
 if __name__ == '__main__':
     data = input()
     floor = 0
+    down = None
 
-    for c in data:
+    for i, c in enumerate(data):
         floor += +1 if c == '(' else -1
 
+        if down is None and floor == -1:
+            down = i + 1
+
+    print('Santa entered the basement at {}'.format(down))
     print('Santa is on floor {}'.format(floor))
