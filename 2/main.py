70b5d0b3b269e52a692983534524668679799850
Lunatrius <jadran.kotnik@gmail.com>
2015-12-24 23:18:16 +0100

    Day 2, part 2

--- 2/main.py
+++ 2/main.py
@@ -4,6 +4,7 @@ import fileinput
 
 if __name__ == '__main__':
     paper = 0
+    ribbon = 0
 
     for line in fileinput.input():
         l, w, h = dimensions = [int(i) for i in line.strip().split('x')]
@@ -11,5 +12,7 @@ if __name__ == '__main__':
         dimensions.sort()
 
         paper += 2 * l * w + 2 * w * h + 2 * h * l + dimensions[0] * dimensions[1]
+        ribbon += 2 * dimensions[0] + 2 * dimensions[1] + l * w * h
 
     print('The elves should order {} square feet of wrapping paper'.format(paper))
+    print('The elves should order {} feet of ribbon'.format(ribbon))
