f59e76aef8a4fd0b970a32546561b5a73415aa34
Lunatrius <jadran.kotnik@gmail.com>
2015-12-24 23:46:31 +0100

    Day 4, part 2

--- 4/main.py
+++ 4/main.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     data = input()
     i = 1
 
-    while hashlib.md5(('{}{}'.format(data, i)).encode('utf-8')).hexdigest()[:5] != '0' * 5:
+    while hashlib.md5(('{}{}'.format(data, i)).encode('utf-8')).hexdigest()[:6] != '0' * 6:
         i += 1
 
     print('The AdventCoin is at {}'.format(i))
