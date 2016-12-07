789484035c920a064128cc583a6e9cc3f8feb504
Lunatrius <jadran.kotnik@gmail.com>
2016-12-07 18:16:59 +0100

    Day 2, part 2

--- 2/main.py
+++ 2/main.py
@@ -1,39 +1,46 @@
 #!/usr/bin/env python
+import operator
 import fileinput
 
 
-def clamp(value, _min, _max):
-    if value < _min:
-        return _min
-
-    if value > _max:
-        return _max
-
-    return value
-
 if __name__ == '__main__':
+    directions = {
+        'U': ( 0, -1),
+        'D': ( 0, +1),
+        'L': (-1,  0),
+        'R': (+1,  0),
+    }
+
     keypad = (
-        (1, 2, 3),
-        (4, 5, 6),
-        (7, 8, 9),
+        (' ', ' ', ' ', ' ', ' ', ' ', ' '),
+        (' ', ' ', ' ', '1', ' ', ' ', ' '),
+        (' ', ' ', '2', '3', '4', ' ', ' '),
+        (' ', '5', '6', '7', '8', '9', ' '),
+        (' ', ' ', 'A', 'B', 'C', ' ', ' '),
+        (' ', ' ', ' ', 'D', ' ', ' ', ' '),
+        (' ', ' ', ' ', ' ', ' ', ' ', ' '),
     )
 
     code = []
 
     for line in fileinput.input():
+        position = (+1, +3)
         x = 1
-        y = 1
+        y = 3
 
         for instr in line.strip():
-            if instr == 'U':
-                y = clamp(y - 1, 0, 2)
-            elif instr == 'D':
-                y = clamp(y + 1, 0, 2)
-            elif instr == 'L':
-                x = clamp(x - 1, 0, 2)
-            elif instr == 'R':
-                x = clamp(x + 1, 0, 2)
+            direction = directions.get(instr)
 
-        code.append(str(keypad[y][x]))
+            if not direction:
+                continue
+
+            position2 = tuple(map(operator.add, position, direction))
+
+            if keypad[position2[1]][position2[0]] == ' ':
+                continue
+
+            position = position2
+
+        code.append(keypad[position[1]][position[0]])
 
     print('Bathroom code: {}'.format(''.join(code)))
