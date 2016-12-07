4e78239ea8aaeae575c85cf17b006e1c838c64c0
Lunatrius <jadran.kotnik@gmail.com>
2016-12-07 18:44:21 +0100

    Day 3, part 2

--- 3/main.py
+++ 3/main.py
@@ -6,13 +6,27 @@ if __name__ == '__main__':
     valid = 0
     total = 0
 
+    triangles = []
+
     for line in fileinput.input():
-        lengths = sorted([int(length) for length in line.strip().split(' ') if length])
+        for length in line.strip().split(' '):
+            if length == '':
+                continue
 
-        if lengths[0] + lengths[1] > lengths[2]:
-            valid += 1
+            triangles.append(int(length))
 
-        total += 1
+    group_size = 9
+    for group in range(0, int(len(triangles) / group_size)):
+        index = group * group_size
+        group_triangles = triangles[index:index+group_size]
+
+        for i in range(0, 3):
+            lengths = sorted(group_triangles[i::3])
+
+            if lengths[0] + lengths[1] > lengths[2]:
+                valid += 1
+
+            total += 1
 
     print('Valid Triangles: {}'.format(valid))
     print('Total Triangles: {}'.format(total))
